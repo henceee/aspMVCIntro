492046505171280066550c5500fc95d4ce5197cf
henceee <hg222dv@student.lnu.se>
2015-11-04 13:15:07 +0100

    .

---
+++ MyValuableCalculator/MyValuableCalculator/Models/ElementaryMath.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyValuableCalculator.Models
+{
+    public enum ArithmeticOperation { Addition, Subtraction, Multiplication, Division}
+
+    public class ElementaryMath
+    {
+        public ArithmeticOperation ArithmeticOperation { get; set; }
+
+        [Required]
+        public int Op1 { get; set; }
+        [Required]
+        public int Op2 { get; set; }
+        public int? Result { get; set; }
+
+        public void Compute()
+        {
+            switch(this.ArithmeticOperation)
+            {
+                case ArithmeticOperation.Addition:
+                    this.Result = this.Op1 + this.Op2;
+                    break;
+
+                case ArithmeticOperation.Subtraction:
+                    this.Result = this.Op1 - this.Op2;
+                    break;  
+
+                case ArithmeticOperation.Multiplication:
+                    this.Result = this.Op1 * this.Op2;
+                    break;
+                
+                case ArithmeticOperation.Division:
+                    this.Result = this.Op1 / this.Op2;
+                    break;
+            }           
+        }
+    }
+}
