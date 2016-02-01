b62e69b188c6979c7d6600ce4d1cb2bd0614d031
henceee <hg222dv@student.lnu.se>
2016-02-01 21:05:43 +0100

    sfsdf #1

---
+++ MyValuableCalculator/Backup/MyValuableCalculator/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyValuableCalculator.Models;
+
+namespace MyValuableCalculator.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public ElementaryMath ElementaryMath {get; set;}
+        public SelectList ArithmeticOperations
+        {            
+            get
+            {
+                var items = new Dictionary<ArithmeticOperation, string>
+                {
+                    {ArithmeticOperation.Addition, "+"},
+                    {ArithmeticOperation.Subtraction, "-"},
+                    {ArithmeticOperation.Multiplication, "*"},
+                    {ArithmeticOperation.Division, "/"}                
+
+                };
+               
+                return new SelectList(items, "Key", "Value");
+            }
+        }
+    }
+}
